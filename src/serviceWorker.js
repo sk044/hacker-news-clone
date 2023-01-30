4155cf17de81d6c45037410e2858697ac38097ff
Shubham Kumar <shubham@tailwnd.com>
2023-01-31 03:42:25 +0530

    init

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -122,3 +122,18 @@ const isLocalhost = Boolean(
     }
   }
   
+  self.addEventListener("activate", function (event) {
+    event.waitUntil(
+      this.caches.keys().then(function (cacheNames) {
+        return Promise.all(
+          cacheNames
+            .filter(function (cacheName) {
+              return true;
+            })
+            .map(function (cacheName) {
+              return caches.delete(cacheName);
+            })
+        );
+      })
+    );
+  });
