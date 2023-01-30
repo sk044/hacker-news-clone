b4c3e0fdb46458745b41ea871977d3a1b6847c1e
Shubham Kumar <shubham@tailwnd.com>
2023-01-31 02:10:15 +0530

    init

--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,11 @@ const Comment = React.lazy(() => import('./views/comments/comments'))
 
 const routes = [
   { path: '/', exact: true, name: 'Recent Stories', element: Home },
+  { path: '/search', exact: true, name: 'Search', element: Search },
   { path: '/search/:searchQuery', exact: true, name: 'Search', element: Search },
+  { path: '/profile', exact: true, name: 'Profile', element: Profile },
   { path: '/profile/:id', exact: true, name: 'Profile', element: Profile },
+  { path: '/comments', exact: true, name: 'Comments', element: Comment },
   { path: '/comments/:id', exact: true, name: 'Comments', element: Comment },
 ]
 
